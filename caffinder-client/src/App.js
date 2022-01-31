94469f92f6ae34096986df076d0a9369b57153e8
Stanley Jeong <stanley.jeong@shopify.com>
2022-01-31 14:52:24 -0800

    initialized React and set up folders

---
+++ caffinder-client/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import LandingPage from './navigation/LandingPage';
+import ActiveView from './navigation/ActiveView';
+
+function App() {
+  return (
+    <>
+      <LandingPage />
+      {/* <ActiveView /> */}
+    </>
+  );
+}
+
+export default App;
---
+++ caffinder-client/src/components/CoffeeShop.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import logo from '../logo.svg';
+import Button from './Button';
+
+export default function CoffeeShop() {
+  return (
+    <>
+      <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>
+          INDIVIDUAL COFFEE SHOP VIEW
+        </h1>
+        <Button customWidth={'70vw'} customText={'Use Current Location'}/>
+        <div style={{marginTop: 20}} >
+          <input 
+            placeholder='Enter Address'
+            style={{ padding: 20, borderRadius: 10, width: '44vw', textAlign: 'center', }} 
+          />
+          <Button onClick={() => console.log('test press')} customWidth={'17vw'} customText={'Search'} color={'black'} textColor={'white'} />
+        </div>
+      </header>
+    </div>
+    </>
+  );
+};
+
+
+
