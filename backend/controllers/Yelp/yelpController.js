696af7237799eba0c9febe6e2912b5a3cc1891fa
Ayush Jain <ayushshreeshreemal@gmail.com>
2022-02-06 22:08:32 -0800

    initial

--- backend/controllers/Yelp/yelpController.js
+++ backend/controllers/Yelp/yelpController.js
@@ -1,24 +1,24 @@
 const yelpBusinesses = {
   get:(req, res) => {
-    console.log('Insert Code Here')
+    console.log('Insert Code Here to get businesses');
   }
 }
 
 const yelpReviews = {
   get:(req, res) => {
-    console.log('Insert Code Here')
+    console.log('Insert Code Here to get reviews');
   }
 }
 
 const yelpDetails = {
   get:(req, res) => {
-    console.log('Insert Code Here')
+    console.log('Insert Code Here to get details');
   }
 }
 
 const yelpSimilar = {
   get:(req, res) => {
-    console.log('Insert Code Here')
+    console.log('Insert Code Here to get similar');
   }
 }
 
