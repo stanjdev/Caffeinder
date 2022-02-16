5f68ca76335f40aa004ead2cfc9622d210001789
OmarJ <omarjandali93@gmail.com>
2022-02-16 11:03:25 -0800

    added google firebase api to gitignore file and installed required package

--- backend/server/www/bin/firebase.js
+++ backend/server/www/bin/firebase.js
@@ -1,5 +1,5 @@
 var admin = require("firebase-admin");
-var serviceAccount = require("../../../caffinder-4e392-firebase-adminsdk-vma96-2885c19c3a.json");
+var serviceAccount = require("../../../caffinder-4e392-firebase-adminsdk-vma96-242717f826.json");
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://caffinder-4e392-default-rtdb.firebaseio.com",
